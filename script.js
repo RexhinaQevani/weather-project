0d7248c92934f3db65cda92d5b6e91708fb0cb0f
RexhinaQevani <reginaqevani@gmail.com>
2021-05-16 19:33:51 +0200

    Implemented the conversion units

--- script.js
+++ script.js
@@ -1,49 +1,34 @@
-let now = new Date();
-let h2 = document.querySelector("h2");
-let date = now.getDate();
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let day = days[now.getDay()];
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-h2.innerHTML = `${day} ${date} ${month}`;
-let h4 = document.querySelector("h4");
-let hours = now.getHours();
-let minutes = ("0" + now.getMinutes()).slice(-2);
-h4.innerHTML = `${hours}:${minutes}`;
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  let tempElement = document.querySelector(".temp-today");
-  tempElement.innerHTML = 21;
-}
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let tempElement = document.querySelector(".temp-today");
-  tempElement.innerHTML = 77;
-}
-let celsiusTemp = document.querySelector("#celsius-temp");
-celsiusTemp.addEventListener("click", convertToCelsius);
-let fahrenheitTemp = document.querySelector("#fahrenheit-temp");
-fahrenheitTemp.addEventListener("click", convertToFahrenheit);
-
 function displayWeatherCondition(response) {
+  let now = new Date();
+  let h2 = document.querySelector("h2");
+  let date = now.getDate();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[now.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  h2.innerHTML = `${day} ${date} ${month}`;
+  let h4 = document.querySelector("h4");
+  let hours = now.getHours();
+  let minutes = ("0" + now.getMinutes()).slice(-2);
+  h4.innerHTML = `${hours}:${minutes}`;
+
   document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector("#current-temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  celsiusTemperature = response.data.main.temp;
+  document.querySelector("#current-temp").innerHTML =
+    Math.round(celsiusTemperature);
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
@@ -60,12 +45,35 @@ function displayWeatherCondition(response) {
     .querySelector("#icon")
     .setAttribute("alt", response.data.weather[0].description);
 }
+function convertToCelsius(event) {
+  event.preventDefault();
+  celsiusTemp.classList.remove("active");
+  fahrenheitTemp.classList.add("active");
+  let tempElement = document.querySelector(".temp-today");
+  tempElement.innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusTemp = document.querySelector("#celsius-temp");
+celsiusTemp.addEventListener("click", convertToCelsius);
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  celsiusTemp.classList.add("active");
+  fahrenheitTemp.classList.remove("active");
+  let tempElement = document.querySelector(".temp-today");
+  tempElement.innerHTML = Math.round((celsiusTemperature * 9) / 5 + 32);
+}
+let fahrenheitTemp = document.querySelector("#fahrenheit-temp");
+fahrenheitTemp.addEventListener("click", convertToFahrenheit);
+
+let celsiusTemperature = null;
+
 let apiKey = "531a64fc7bab71506a1897c786ce4532";
 let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
 function searchCity(city) {
   let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
+
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#search-text-input").value;
