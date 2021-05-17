63e202e4c0e9e29bdd723398846966f8847bb4eb
RexhinaQevani <reginaqevani@gmail.com>
2021-05-18 00:05:56 +0200

    Final touches
    Removed Fahrenheit conversion, added min-max temp in current temperature element, added description in forecast.

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let apiKey = "531a64fc7bab71506a1897c786ce4532";
+let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+
+function searchCity(city) {
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let apiUrl = `${apiEndpoint}?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentPositionButton = document.querySelector(".current-position");
+currentPositionButton.addEventListener("click", getCurrentLocation);
+
 function displayWeatherCondition(response) {
   let now = new Date();
   let h2 = document.querySelector("h2");
@@ -26,9 +54,15 @@ function displayWeatherCondition(response) {
   h4.innerHTML = `${hours}:${minutes}`;
 
   document.querySelector("#city-name").innerHTML = response.data.name;
-  celsiusTemperature = response.data.main.temp;
-  document.querySelector("#current-temp").innerHTML =
-    Math.round(celsiusTemperature);
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#min-temp").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#max-temp").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
@@ -47,10 +81,9 @@ function displayWeatherCondition(response) {
 
   getForecast(response.data.coord);
 }
+
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -63,10 +96,8 @@ function formatDay(timestamp) {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
-  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-
   forecast.forEach(function (forecastDay, index) {
     if (index > 0 && index < 7) {
       forecastHTML =
@@ -78,71 +109,23 @@ function displayForecast(response) {
                 <img src="http://openweathermap.org/img/wn/${
                   forecastDay.weather[0].icon
                 }@2x.png" alt="" width="80px"/>
+                <div class="forecast-description">${
+                  forecastDay.weather[0].description
+                }</div>
                 <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-min"> ${Math.round(
+                  forecastDay.temp.min
+                )}° </span> - 
                <span class="weather-forecast-temperature-max"> ${Math.round(
                  forecastDay.temp.max
                )}° </span>
-               <span class="weather-forecast-temperature-min"> ${Math.round(
-                 forecastDay.temp.min
-               )}° </span>
+
               </div>
-             <button type="button" class="btn btn-outline-warning">
-                    More Info
-                  </button>
               </div>`;
     }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  celsiusTemp.classList.remove("active");
-  fahrenheitTemp.classList.add("active");
-  let tempElement = document.querySelector(".temp-today");
-  tempElement.innerHTML = Math.round(celsiusTemperature);
-}
-let celsiusTemp = document.querySelector("#celsius-temp");
-celsiusTemp.addEventListener("click", convertToCelsius);
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  celsiusTemp.classList.add("active");
-  fahrenheitTemp.classList.remove("active");
-  let tempElement = document.querySelector(".temp-today");
-  tempElement.innerHTML = Math.round((celsiusTemperature * 9) / 5 + 32);
-}
-let fahrenheitTemp = document.querySelector("#fahrenheit-temp");
-fahrenheitTemp.addEventListener("click", convertToFahrenheit);
-
-let celsiusTemperature = null;
-
-let apiKey = "531a64fc7bab71506a1897c786ce4532";
-let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-function searchCity(city) {
-  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#search-text-input").value;
-  searchCity(city);
-}
-function searchLocation(position) {
-  let apiUrl = `${apiEndpoint}?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
-
-let currentPositionButton = document.querySelector(".current-position");
-currentPositionButton.addEventListener("click", getCurrentLocation);
 
 searchCity("Verona");
